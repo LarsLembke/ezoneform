27e3284eba8bec963a43c6033eeedf4ba19fbf0c
larslembke <lars46i6@stud.kea.dk>
2021-11-08 14:13:08 +0100

    js crud settings

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+import {
+    get,
+    post,
+    put,
+    deleteIt
+} from "./js/crud.js";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+
+    console.log("stärt");
+
+}
