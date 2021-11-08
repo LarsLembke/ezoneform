26804885a37c41046ce23b450e3b9e67f6ffdcb8
larslembke <lars46i6@stud.kea.dk>
2021-11-08 15:39:31 +0100

    data js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-"use strict";
-
 import {
     get,
     post,
     put,
     deleteIt
-} from "./js/crud.js";
+}
+from "./js/crud.js";
 
-window.addEventListener("DOMContentLoaded", start);
+document.addEventListener("DOMContentLoaded", start);
 
 function start() {
 
     console.log("stärt");
+
+    const submitButton;
+    const form = document.querySelector("form");
+
+    submitButton.addEventListener("mousedown", readData);
+
+    function readData() {
+        const data = {
+            firstname: form.elements.firstname.value,
+            lastname: form.elements.lastname.value,
+            gamertag: form.elements.gamertag.value,
+            dob: form.elements.dob.value,
+            email: form.elements.email.value,
+            password: form.elements.password.value,
+            gamingstyle: form.elements.gaming_style.value,
+            games: form.elements.game.value,
+            strength: form.elements.strength.value,
+            improvement: form.elements.improvement.value,
+            service: form.elements.service.value
+        }
+    }
 
 }
